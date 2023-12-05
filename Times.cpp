aca05bffc0e5f881359d0bc53c92631e45d9d282
FanXinDai <151131867+FanXinDai@users.noreply.github.com>
2023-12-05 23:10:18 +0800

    second

--- Times.cpp
+++ Times.cpp
@@ -1,17 +1,17 @@
 #include <stdio.h>
 int main()
 {
-	int m1,m2,m3,m4,m5,s1,s2,s3,s4,s5,total,total_1,total_2;
-	scanf("%d %d",&m1,&s1);
-	scanf("%d %d",&m2,&s2);
-	scanf("%d %d",&m3,&s3);
-	scanf("%d %d",&m4,&s4);
-	scanf("%d %d",&m5,&s5);
-	total_1=m1+m2+m3+m4+m5;
-	total_2=s1+s2+s3+s4+s5;
-	total=total_2/60;
-	total_2=total_2%60;
-	total_1=total_1+total;
-	printf("%d : %d",total_1,total_2);
+	int i,j,hour,minute,total_h=0,total_m=0;
+	
+	for(i=1;i<=5;i++)
+	{
+		scanf("%d %d",&hour,&minute);
+		total_h=total_h+hour;
+		total_m=total_m+minute;                                           
+	} 
+	hour=total_m/60;
+	minute=total_m%60;	
+	hour=hour+total_h;
+	printf("%d : %d",hour,minute);  
 	return 0;
 } 
