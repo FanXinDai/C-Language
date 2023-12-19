fe5a888c210f41168fc3d6e3c1bbc9db397657fd
FanXinDai <151131867+FanXinDai@users.noreply.github.com>
2023-12-19 20:07:43 +0800

    Add files via upload

---
+++ Determine_whether_the_class_grade_is_high_or_low.cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+	int num,i,grade,maxi=-100,mini=100,total=0;
+	printf("Please enter the number of students in the class :");
+	scanf("%d",&num);
+	for(i=1;i<=num;i++)
+	{
+		printf("Please enter the grade for student number %d :",i);
+		scanf("%d",&grade);
+		if(grade>maxi)
+		{
+			maxi=grade;
+		}
+		if(grade<mini)
+		{
+			mini=grade;
+		}
+		
+	}
+	printf("The highest score is %d points \n The lowest score is %d points",maxi,mini);
+ } 
